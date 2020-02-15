34d922d62ef733a25f7aa2e11c6e7280a2eb1ca7
Leila Hsiao <lsiao@zipcodes-MacBook-Pro-6.local>
2020-02-15 00:21:11 -0500

    finished

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -1,5 +1,4 @@
 package io.zipcoder;
-
 public class Music {
 
     private String[] playList;
@@ -7,8 +6,49 @@ public class Music {
     public Music(String[] playList){
         this.playList = playList;
     }
-
     public Integer selection(Integer startIndex, String selection){
-        return null;
+
+        int toRight = findNextSong(startIndex, selection);
+        int toLeft = findPrevSong(startIndex, selection);
+
+        //return (toRight > toLeft)? toLeft: toRight;
+        return Math.min(toRight, toLeft);
     }
+
+    public int findNextSong(int index, String songName){
+        int i = index;
+        int steps = 0;
+        do{
+            if(playList[i].equals(songName)){
+                return steps;
+            }
+            i++;
+            steps++;
+
+            if(i==playList.length){
+                i=0;
+            }
+        }while(i != index);
+        return -1;
+    }
+
+    public int findPrevSong(int index, String songName){
+        int i = index;
+        int steps = 0;
+        do{
+            if(playList[i].equals(songName)){
+                return steps;
+            }
+            i--;
+            steps++;
+
+            if(i==-1){
+                i=playList.length-1;
+            }
+        }while(i != index);
+        return -1;
+    }
+
+
+
 }
--- src/test/java/io/zipcoder/MusicTest.java
+++ src/test/java/io/zipcoder/MusicTest.java
@@ -5,9 +5,9 @@ import org.junit.Test;
 
 public class MusicTest {
 
+
     @Test
     public void selectionTest1(){
-
         String[] playlist = {"wheniseeyouagain","borntorun","nothingelsematters","cecelia"};
         Music music = new Music(playlist);
         Integer startingIndex = 1;
@@ -28,4 +28,92 @@ public class MusicTest {
         Integer actual = music.selection(startingIndex, selection);
         Assert.assertEquals(expected, actual);
     }
+
+    @Test
+    public void selectionTest3(){
+
+        String[] playlist = {"a","b","c","d","e"};
+        Music music = new Music(playlist);
+        Integer startingIndex = 3;
+        String selection = "a";
+        Integer expected = 2;
+        Integer actual = music.selection(startingIndex, selection);
+        Assert.assertEquals(expected, actual);
+    }
+
+
+    String[] playlist1 = {"A","c","c","c","c","A","A","c","c","c"};
+    @Test
+    public void selectionTest4(){
+        Music music = new Music(playlist1);
+        Integer startingIndex = 1;
+        String selection = "A";
+        Integer expected = 1;
+        Integer actual = music.selection(startingIndex, selection);
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void selectionTest5(){
+        Music music = new Music(playlist1);
+        Integer startingIndex = 8;
+        String selection = "A";
+        Integer expected = 2;
+        Integer actual = music.selection(startingIndex, selection);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void selectionTest6(){
+        Music music = new Music(playlist1);
+        Integer startingIndex = 3;
+        String selection = "A";
+        Integer expected = 2;
+        Integer actual = music.selection(startingIndex, selection);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void findNextSongTest1(){
+        String[] playlist = {"a","b","c","d","e"};
+        Music music = new Music(playlist);
+        Integer startingIndex = 4;
+        String selection = "d";
+        Integer expected = 4;
+        Integer actual = music.findNextSong(startingIndex, selection);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void findNextSongTest2(){
+        String[] playlist = {"a","b","c","d","e"};
+        Music music = new Music(playlist);
+        Integer startingIndex = 1;
+        String selection = "d";
+        Integer expected = 2;
+        Integer actual = music.findNextSong(startingIndex, selection);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void findPrevSongTest1(){
+        String[] playlist = {"a","b","c","d","e"};
+        Music music = new Music(playlist);
+        Integer startingIndex = 0;
+        String selection = "d";
+        Integer expected = 2;
+        Integer actual = music.findPrevSong(startingIndex, selection);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void findPrevSongTest2(){
+        String[] playlist = {"a","b","c","d","e"};
+        Music music = new Music(playlist);
+        Integer startingIndex = 4;
+        String selection = "b";
+        Integer expected = 3;
+        Integer actual = music.findPrevSong(startingIndex, selection);
+        Assert.assertEquals(expected, actual);
+    }
+
 }
